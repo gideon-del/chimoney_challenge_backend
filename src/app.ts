ae89c3a1429290613265ff9cb7b0e7ff925fd266
Gideon Chidi <gideonchidi471@gmail.com>
2024-05-05 11:00:32 +0100

    feat: added register route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import helmet from "helmet";
+import authRoute from "./routes/auth.route";
 const app = express();
 
 app.use(
@@ -12,4 +13,6 @@ app.use(
 app.use(helmet());
 app.use(morgan("combined"));
 app.use(express.json());
+// Routes
+app.use("/auth", authRoute);
 export default app;
