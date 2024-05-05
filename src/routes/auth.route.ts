295bb5a4f29f5ae1429b7a5f6f61e7bf3b17b7e0
Gideon Chidi <gideonchidi471@gmail.com>
2024-05-05 21:11:34 +0100

    chore: cleaning up

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,40 +2,10 @@ import { Router } from "express";
 import { registerUserSchema } from "../utils/validator";
 import supabase from "../models/supabase";
 import { createHashPassword, createTokens } from "../utils/auth";
+import { createAccount } from "../controllers/auth.controller";
 
 const authRoute = Router();
 
-authRoute.post("/register", async (req, res) => {
-  const user = registerUserSchema.safeParse(req.body);
-  if (!user.success) {
-    const errorMessages = user.error.issues.map((issues) => ({
-      message: `${issues.path.join(".")} is ${issues.message}`,
-    }));
-    return res.status(400).json(errorMessages);
-  }
-  // Check if user already exists
-  const { data: userExists } = await supabase
-    .from("users")
-    .select()
-    .eq("email", user.data.email);
-  if (userExists && userExists.length > 0) {
-    return res.status(400).json({
-      message: "Email already taken",
-    });
-  }
-  //   Create a hash password and save to db
-  const hashedPassword = createHashPassword(user.data.password);
-  const { data, error } = await supabase
-    .from("users")
-    .insert({
-      ...user.data,
-      password: hashedPassword,
-    })
-    .select();
-
-  const userId = data![0]["id"] as string;
-  const tokens = createTokens(userId);
-  return res.status(200).json(tokens);
-});
+authRoute.post("/register", createAccount);
 
 export default authRoute;
